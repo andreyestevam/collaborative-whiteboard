d4b2d9c43634a5f4b45ab803d8f68c37ddda499a
Andrey Estevam Seabra <profissional.drey@gmail.com>
2024-12-30 01:13:52 -0500

    Added Spring Boot files.

---
+++ src/main/java/collaborative/whiteboard/collaborative_whiteboard/CollaborativeWhiteboardApplication.java
@@ -0,0 +1,13 @@
+package collaborative.whiteboard.collaborative_whiteboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeWhiteboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeWhiteboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/collaborative/whiteboard/collaborative_whiteboard/CollaborativeWhiteboardApplicationTests.java
@@ -0,0 +1,13 @@
+package collaborative.whiteboard.collaborative_whiteboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeWhiteboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
